492f490f4b56665d369a2307708bc9d3336c7dec
bruno.a.da.silva <bruno.a.da.silva@accenture.com>
2018-09-25 20:28:07 -0300

    Paciencia, muita paciencia

--- app/src/main/java/com/fatec/tcc/findfm/Model/Business/Post.java
+++ app/src/main/java/com/fatec/tcc/findfm/Model/Business/Post.java
@@ -1,6 +1,9 @@
 package com.fatec.tcc.findfm.Model.Business;
 
+import com.fatec.tcc.findfm.Model.Http.Response.PostResponse;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -12,11 +15,43 @@ public class Post {
     private String descricao;
     private String cidade;
     private String idFoto;
+    private List<String> idFotos;
     private String idVideo;
+    private List<String> idVideos;
     private Date data;
+    private Long likes;
     private List<Comentario> comentarios;
 
     public Post(){}
+
+    public Post(PostResponse postResponse){
+        this.setIdFotos(new ArrayList<>());
+        this.setIdVideos(new ArrayList<>());
+        this.titulo = postResponse.getTitulo();
+        this.descricao = postResponse.getDescricao();
+        //TODO: talvez colocar
+        //this.cidade
+        //this.UF
+        for(PostResponse.Midias midia: postResponse.getMidias()){
+            if(midia.getTipoMidia().equals("img"))
+                this.getIdFotos().add(midia.getId());
+            else
+                this.getIdVideos().add(midia.getId());
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setId(postResponse.getAutor().get_id());
+        usuario.setFotoID(postResponse.getAutor().getAvatar().get_id());
+        usuario.setTipoUsuario(TiposUsuario.fromKind(postResponse.getAutor().getKind()));
+        usuario.setNomeCompleto(postResponse.getAutor().getFullName());
+        this.autor = usuario;
+        //TODO: colocar esses campos
+        this.data = postResponse.getData();
+        usuario.setEmail(postResponse.getAutor().getEmail());
+        if(postResponse.getAutor().getTelefone() != null)
+            usuario.setTelefone(postResponse.getAutor().getTelefone().getStateCode() + postResponse.getAutor().getTelefone().getNumber());
+
+    }
 
     public String getTitulo() {
         return titulo;
@@ -65,10 +100,10 @@ public class Post {
 
     public String getData() {
         if(data != null) {
-            return new SimpleDateFormat("dd-MM-yyyy' 'HH:mm:ss", Locale.US).format(data);
+            return new SimpleDateFormat("dd/MM/yyyy' 'HH:mm:ss", Locale.US).format(data);
         }else
         {
-            return new SimpleDateFormat("dd-MM-yyyy' 'HH:mm:ss", Locale.US).format(new Date());
+            return new SimpleDateFormat("dd/MM/yyyy", Locale.US).format(new Date());
         }
     }
 
@@ -93,5 +128,29 @@ public class Post {
     public Post setIdVideo(String idVideo) {
         this.idVideo = idVideo;
         return this;
+    }
+
+    public List<String> getIdFotos() {
+        return idFotos;
+    }
+
+    public void setIdFotos(List<String> idFotos) {
+        this.idFotos = idFotos;
+    }
+
+    public List<String> getIdVideos() {
+        return idVideos;
+    }
+
+    public void setIdVideos(List<String> idVideos) {
+        this.idVideos = idVideos;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Long likes) {
+        this.likes = likes;
     }
 }
